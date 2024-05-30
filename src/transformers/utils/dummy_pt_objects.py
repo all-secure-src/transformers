81bb8a5ae44559da6c39888ce7c54bf7f2f0d877
apt-team <code.x.0018@gmail.com>
2024-05-30 11:32:12 +0530

    New changes

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -4710,6 +4710,19 @@ class LlavaNextPreTrainedModel(metaclass=DummyObject):
     def __init__(self, *args, **kwargs):
         requires_backends(self, ["torch"])
 
+class OmegaSparkVisionForConditionalGeneration(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class OmegaSparkVisionPreTrainedModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
 
 class LongformerForMaskedLM(metaclass=DummyObject):
     _backends = ["torch"]
